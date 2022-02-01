886c625a54b39e98dc09ab8af740f23c50d62e36
MaximilianoBidcom <maximilianocabrerap@gmail.com>
2022-02-01 00:34:43 -0300

    Carrito v0.2
    Agrego icons

--- components/Product.jsx
+++ components/Product.jsx
@@ -4,6 +4,10 @@ import { useDispatch } from 'react-redux';
 
 import { addToCart, removeFromCart, reduceFromCart, sumToCart } from '../features/Cart';
 
+
+import { GrAddCircle, GrSubtractCircle, GrTrash } from 'react-icons/gr';
+
+
 export default function Product({ product }) {
     const dispatch = useDispatch();
     const agregarAlCarrito = () => {
@@ -18,6 +22,7 @@ export default function Product({ product }) {
     const sumarAlCarrito = () => {
         dispatch(sumToCart(product))
     }
+    let classSubtract = product.quantity > 1 ? "" : "";
     return <motion.article
         initial={{
             opacity: 0
@@ -34,10 +39,16 @@ export default function Product({ product }) {
             <p className='my-3'>{product.description ?? product.consola}</p>
             {product.quantity ?
                 <>
-                    Cantidad:  {product.quantity} <br />
-                    <button onClick={restarDelCarrito}>-</button><br />
-                    <button onClick={removerDelCarrito}>Remover del Carrito</button> <br />
-                    <button onClick={sumarAlCarrito}>+</button>
+                    <div className="flex justify-between">
+                        <div className="">
+                            <button onClick={restarDelCarrito} disabled={product.quantity == 1} className="mx-2 my-1 disabled:opacity-60"><GrSubtractCircle /></button>
+                            Cantidad:  {product.quantity}
+                            <button onClick={sumarAlCarrito} disabled={product.stock == product.quantity} className="mx-2 my-1 disabled:opacity-60"><GrAddCircle /></button>
+                        </div>
+                        <div >
+                            <button onClick={removerDelCarrito} className="my-2 my-1 text-xs text-red-600">Eliminar </button>
+                        </div>
+                    </div>
                 </> :
                 <>
                     <button onClick={agregarAlCarrito}>Agregar al Carrito</button>
